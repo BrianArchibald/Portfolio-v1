206979dbc7ef9d1691133f074a2b5988f9c7e973
BrianArchibald <brianallenarchibald@gmail.com>
2018-06-28 19:23:37 -0500

    more css, transitions, changed color

--- portfolio.js
+++ portfolio.js
@@ -11,3 +11,37 @@ function scrolledNav () {
 
 window.addEventListener('scroll', scrolledNav);
 
+
+// will run checkSlide every 20ms so checkSlide is not running constantly
+function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+const slideUpBoxes = document.querySelectorAll('.slideUp');
+const slideProjects = document.querySelectorAll('.projects');
+
+function checkSlide() {     
+	 for (var i=0; i<slideUpBoxes.length; i++) {
+	 	if (window.scrollY > 220) {
+	 		slideUpBoxes[i].classList.add('activeSkills');
+	 	}
+	  }
+	  for (var i=0; i<slideProjects.length; i++) {
+	  	if (window.scrollY > 670) {
+	  		slideProjects[i].classList.add('projectActive');
+	  	}
+	  }
+	}
+
+window.addEventListener('scroll', debounce(checkSlide));
