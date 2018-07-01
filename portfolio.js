589cfaeb3496228fa55624f9b96b97e0709632d9
BrianArchibald <brianallenarchibald@gmail.com>
2018-07-01 14:32:48 -0500

    added skills icons and skills section, adjusted navbar,

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,15 @@
 const nav = document.querySelector('.nav-sticky');
 
-// function scrolledNav () {
-// 	if (window.scrollY > 0) {
-// 		document.getElementById("navSticky").classList.add("nav-sticky-v2");
-// 	}
-// 	else {
-// 		document.getElementById("navSticky").classList.remove("nav-sticky-v2");
-// 	}
-// }
+function scrolledNav () {
+	if (window.scrollY > 0) {
+		document.getElementById("navSticky").classList.add("nav-sticky-v2");
+	}
+	else {
+		document.getElementById("navSticky").classList.remove("nav-sticky-v2");
+	}
+}
 
-// window.addEventListener('scroll', scrolledNav);
+window.addEventListener('scroll', scrolledNav);
 
 
 
