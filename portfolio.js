beabcd21a40b477c4b6891b7b0b7c10a8ac15613
BrianArchibald <brianallenarchibald@gmail.com>
2018-07-15 12:43:35 -0500

    js for mobile nav

--- portfolio.js
+++ portfolio.js
@@ -94,9 +94,20 @@ window.addEventListener('scroll', debounce(checkSlide));
 /*  Mobile Navigation  */
 
 const mobileNav = document.getElementById('menu-icon');
+const mobileNavX = 
+const navList = 
+
 
 function changeMobileNav() {
 	console.log("nav clicked");
+
+	//add and remove class to show icon or show nav options,
+	element.style.display = none;
+	element.style.display = block;
+	// then show x when nav is clicked
+	x.style.display = inline-block;
+	mobileNav.style.display = none;
+	// then when x is clicked show nav again.
 }
 
 
